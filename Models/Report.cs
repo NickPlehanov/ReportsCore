a217c63c410db1f88f5b214d473b21a70ddde43f
Nick Plehanov <nick.plehanov74@gmail.com>
2020-10-06 18:01:33 +0500

    1

--- Models/Report.cs
+++ Models/Report.cs
@@ -1,16 +1,18 @@
-﻿namespace ReportsCore.Models {
+﻿using System;
+
+namespace ReportsCore.Models {
 	public class Report {
 		public Report() {
 		}
 
-		public string ObjectNumber { get; set; }
+		public int? ObjectNumber { get; set; }
 		public string ObjectName { get; set; }
 		public string ObjectAddress { get; set; }
 		public string WhoChanged { get; set; }
-		public string DateChanged { get; set; }
+		public DateTime? DateChanged { get; set; }
 		public string Before { get; set; }
 		public string After { get; set; }
-		public string DateStart { get; set; }
+		public DateTime? DateStart { get; set; }
 		public string Curator { get; set; }
 	}
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,14 +6,13 @@ using ReportsCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO.Compression;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Windows.Documents;
+using System.Windows;
 
 namespace ReportsCore.ViewModels {
 	class MainWindowViewModel : BaseViewModel {
+
+		ObservableCollection<Report> FullReports = new ObservableCollection<Report>();
 
 		private bool _FlyoutMenuState;
 		public bool FlyoutMenuState {
@@ -28,6 +27,8 @@ namespace ReportsCore.ViewModels {
 		public bool FlyoutSettingVisibleState {
 			get => _FlyoutSettingVisibleState;
 			set {
+				if(value)
+					FlyoutMenuState = false;
 				_FlyoutSettingVisibleState = value;
 				OnPropertyChanged("FlyoutSettingVisibleState");
 			}
@@ -82,6 +83,15 @@ namespace ReportsCore.ViewModels {
 			}
 		}
 
+		private string _FilterParameter;
+		public string FilterParameter {
+			get => _FilterParameter;
+			set {
+				_FilterParameter = value;
+				OnPropertyChanged("FilterParameter");
+			}
+		}
+
 		private RelayCommand _MenuOpen;
 		public RelayCommand MenuOpen {
 			get => _MenuOpen ??= new RelayCommand(obj => {
@@ -102,7 +112,47 @@ namespace ReportsCore.ViewModels {
 				FlyoutSettingColumnsVisibleState = FlyoutSettingColumnsVisibleState ? false : true;
 			});
 		}
+		private RelayCommand _FilterOpen;
+		public RelayCommand FilterOpen {
+			get => _FilterOpen ??= new RelayCommand(obj => {
+				FlyoutSettingVisibleState = FlyoutSettingVisibleState ? false : true;
+			});
+		}
+		private RelayCommand _Search;
+		public RelayCommand Search {
+			get => _Search ??= new RelayCommand(obj => {
+				Reports = FullReports;
+				if(!string.IsNullOrEmpty(FilterParameter) || !string.IsNullOrWhiteSpace(FilterParameter)) {
+					bool isDigit = false;
+					char[] filter = FilterParameter.ToCharArray();
+					foreach(char item in filter) {
+						if(char.IsDigit(item))
+							isDigit = true;
+						else {
+							isDigit = false;
+							break;
+						}
+					}
+					//TODO: Проверить что второй раз ищется
+					//FullReports = Reports;
+					if(isDigit)
+						Reports = new ObservableCollection<Report>(Reports.Where(x => x.ObjectNumber.ToString().Contains(FilterParameter)));
+					else
+						Reports = new ObservableCollection<Report>(Reports.Where(x => x.ObjectAddress.ToLower().Contains(FilterParameter.ToLower()) || x.ObjectName.ToLower().Contains(FilterParameter.ToLower())));
+					OnPropertyChanged("Reports");
+				}
+				else
+					MessageBox.Show("Значение для фильтрации не может быть пустым");
+			});
+		}
 
+		private RelayCommand _ClearFilter;
+		public RelayCommand ClearFilter {
+			get => _ClearFilter ??= new RelayCommand(obj => {
+				FilterParameter = null;
+				Reports = FullReports;
+			});
+		}
 		private RelayCommand _SelectDatePattern;
 		public RelayCommand SelectDatePattern {
 			get => _SelectDatePattern ??= new RelayCommand(obj => {
@@ -115,40 +165,67 @@ namespace ReportsCore.ViewModels {
 		private RelayCommand _GetData;
 		public RelayCommand GetData {
 			get => _GetData ??= new RelayCommand(async obj => {
+				//List<Report> report = new List<Report>();
 				using(Vityaz_MSCRMContext context = new Vityaz_MSCRMContext()) {
 					//TODO: Перенести в get
 					NewGuardObjectHistory before = null;
 					NewGuardObjectHistory after = null;
-					DateTime start = DateTime.Parse(DateStart.ToShortDateString());
-					DateTime end = DateTime.Parse(DateEnd.ToShortDateString());
-					List<NewGuardObjectHistory> history = await context.NewGuardObjectHistory.Where(x => x.ModifiedOn >= start && x.ModifiedOn <= end/* && x.NewObjectNumber == 7640*/).ToListAsync<NewGuardObjectHistory>();
+					DateTime start = DateTime.Parse(DateStart.ToShortDateString()).AddHours(-5);
+					DateTime end = DateTime.Parse(DateEnd.ToShortDateString()).AddHours(-5);
+					List<NewGuardObjectHistory> history = await context.NewGuardObjectHistory.Where(x => x.ModifiedOn >= start && x.ModifiedOn <= end /*&& x.NewObjectNumber == 2866*/).ToListAsync<NewGuardObjectHistory>();
 					var r = history.GroupBy(a => new { a.NewGuardObjectId, a.ModifiedBy, DateTime = DateTime.Parse(a.ModifiedOn.ToString()) }).ToList();
 					foreach(var item in r) {
+						before = null;
+						after = null;
 						foreach(var i in item)
 							if(i.HistoryState == "Старый")
 								before = i;
 							else
 								after = i;
 						List<Comparator> t = CompareObject(before, after);
-						if(t.Any()) {
-							string WhoChanged = context.SystemUserBase.FirstOrDefault(x => x.ModifiedBy == after.ModifiedBy).FullName;
-							Guid? CuratorId = context.NewGuardObjectExtensionBase.FirstOrDefault(x => x.NewGuardObjectId == after.NewGuardObjectId).NewCurator;
-							string curatorName = null;
-							if(CuratorId.HasValue) {
-								Guid _id = Guid.Empty;
-								if(Guid.TryParse(CuratorId.Value.ToString(), out _id)) {
-									curatorName = context.SystemUserBase.FirstOrDefault(x => x.SystemUserId == CuratorId).FullName;
+						if(t != null)
+							if(t.Any()) {
+								string WhoChanged = context.SystemUserBase.FirstOrDefault(x => x.SystemUserId == after.ModifiedBy).FullName;
+								Guid? CuratorId = context.NewGuardObjectExtensionBase.FirstOrDefault(x => x.NewGuardObjectId == after.NewGuardObjectId).NewCurator;
+								string curatorName = null;
+								if(CuratorId.HasValue) {
+									Guid _id = Guid.Empty;
+									if(Guid.TryParse(CuratorId.Value.ToString(), out _id)) {
+										curatorName = context.SystemUserBase.FirstOrDefault(x => x.SystemUserId == CuratorId).FullName;
+									}
 								}
+								DateTime? WhenChanged = after.ModifiedOn;
+								string oldValue = null;
+								string newValue = null;
+								foreach(Comparator c in t) {
+									oldValue = c.OldValue;
+									newValue = c.NewValue;
+								}
+								NewGuardObjectExtensionBase objectExtensionBase = context.NewGuardObjectExtensionBase.FirstOrDefault(x => x.NewGuardObjectId == after.NewGuardObjectId);
+								if(objectExtensionBase != null)
+									Reports.Add(new Report() {
+										After = oldValue,
+										Before = newValue,
+										Curator = curatorName,
+										DateChanged = WhenChanged,
+										DateStart = objectExtensionBase.NewDateStart,
+										WhoChanged = WhoChanged,
+										ObjectAddress = objectExtensionBase.NewAddress,
+										ObjectName = objectExtensionBase.NewName,
+										ObjectNumber = objectExtensionBase.NewObjectNumber
+									});
 							}
-						}
 					}
 				}
+				FlyoutMenuState = false;
+				FlyoutSettingVisibleState = false;
+				FullReports = Reports;
 			});
 		}
 
 		private List<Comparator> CompareObject(NewGuardObjectHistory _old, NewGuardObjectHistory _new) {
 			List<Comparator> comparator = new List<Comparator>();
-			if(_old == null && _new == null)
+			if(_old == null || _new == null)
 				return null;
 			else {
 				foreach(var item in _old.GetType().GetProperties()) {
@@ -218,7 +295,7 @@ namespace ReportsCore.ViewModels {
 			get => _Reports;
 			set {
 				_Reports = value;
-				OnPropertyChanged("Report");
+				OnPropertyChanged("Reports");
 			}
 		}
 	}
